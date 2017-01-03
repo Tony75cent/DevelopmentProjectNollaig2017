04602208a8ae4901c4728f7fc4f0087df33df95c
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-01-03 20:15:18 +0000

    Copied a few more files from the Year4project into this one

--- JS/functions.js
+++ JS/functions.js
@@ -73,3 +73,53 @@ function registerUser() {
     return false;
 }
 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function startup() {
+    var graphs = document.getElementById("graphs");
+    tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function (d) {
+                return "<strong>Population:</strong> <span style='color:red'>" + d.value + "</span>";
+            });
+/* For loop */
+    for (i = 1; i < 31; i++) {
+        var div = document.getElementById("graph" + i);
+        if (div === null) {
+            div = document.createElement("div");
+            div.setAttribute("id", "graph" + i);
+            div.setAttribute("class", "svg-container");
+            div.setAttribute("tabindex", "0");
+            div.setAttribute("onclick", "graphClicked(this)");
+            graphs.appendChild(div);
+        }
+        var img = document.createElement("img");
+        img.setAttribute("src", "load.svg");
+        div.appendChild(img);
+        graphs.appendChild(div);
+        getPopulationByCounty(i, div.getAttribute("id"));
+    }
+}
+
+function graphClicked(graph) {
+    var graphId = graph.getAttribute("id");
+    var countyId = graphId.substring(5);
+    console.log(countyId);
+    var focus = document.getElementById("focus");
+    focus.innerHTML = "";
+    element = document.createElement("div");
+    element.setAttribute("class", "close");
+    element.setAttribute("onclick", "focusOff()");
+    element.setAttribute("tabindex", "0");
+    focus.appendChild(element);
+    getPopulationByCounty(countyId, "focus");
+    focus.setAttribute("class", "active");
+}
+
+function focusOff(element) {
+    document.getElementById("focus").setAttribute("class", "inactive");
+}
---
+++ PHP/counties.php
@@ -0,0 +1,44 @@
+
+<?php 
+session_start(true);
+if (!isset($_SESSION['username'])) {
+    header('Location: http://' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER[SERVER_PORT] . "/year4project/", true, 303);
+    exit();
+}
+?>
+<!--This has been copied into the DevelopmentProjectNollaig2017 folder -->
+
+<!doctype html> 
+<html min-height="100%">
+    <head>
+        <meta charset="utf-8">
+        <title>fill with county's</title>
+        <link href="css/basic.css" rel="stylesheet" type="text/css"/>
+        <link href="css/tooltip.css" rel="stylesheet" type="text/css"/>
+        <link href="css/graphs.css" rel="stylesheet" type="text/css"/>
+        <link href="css/layout.css" rel="stylesheet" type="text/css"/>
+        <script src="JS/d3.v3.js"></script>
+        <script src="JS/d3.tip.v0.6.3.js" type="text/javascript"></script>
+        <script src="JS/d3pie.js" type="text/javascript"></script>
+        <script src="JS/functions.js" type="text/javascript"></script>
+        <script src="js/D3_functions.js" type="text/javascript"></script>
+        <script language = "javascript"  src= "js/jsfunctions.js"></script>
+    </head>
+    <body onload="startup();">
+        <div class="top">
+            <div class="graphicons">
+                <fieldset name="graphtype">
+                    <input id="piebutton" type="radio" name="pie" checked="checked" onchange="startup();"/>
+                    <label for="piebutton" class="pie"><img src="piechart.svg" class="icon"/></label>
+                    <input id="barbutton" type="radio" name="pie" onchange="startup();"/>
+                    <label for="barbutton" class="bar"><img src="barchart.svg" class="icon"/></label>
+                </fieldset>
+            </div>
+            <h1 style="margin: 5px; text-align: center">County Population by Barony</h1>
+        </div>
+        <div id="graphs" class="graphs"></div>   
+        <div id="focus" class="inactive"></div>
+    </body>
+</html>
+
+
