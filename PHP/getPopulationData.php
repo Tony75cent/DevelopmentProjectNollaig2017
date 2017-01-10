a35b1945ca21242a5621512c9c6e0a10bf50856a
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-01-10 12:10:33 +0000

    Removed an unnessary comma in the SELECT statment

--- PHP/getPopulationData.php
+++ PHP/getPopulationData.php
@@ -6,9 +6,9 @@ mysqli_select_db($connection, "$db_name")or die("cannot select a DB");
 /** $countyid = $_GET["countyId"]; **/ /**This line works it has been commented out for testing **/
 
 /** $sql ="SELECT `COUNTY_ID`, `GEOGDESC`, `CSOBARNAME`, `Male2011`, `Female2011`, `Total2011` FROM `baronys` WHERE `COUNTY_ID`=".$countyid; **/ /** This is the original line **/
-$sql ="`COUNTY,C,5`,`COUNTYNAME,C,35`,`CSOBARNAME,C,28`,`Total2011,N,20,10`,`Male2011,N,20,10`,`Female2011,N,20,10`, FROM `baronies` WHERE `COUNTY,C,5`='33'";
+$sql ="`COUNTY,C,5`,`COUNTYNAME,C,35`,`CSOBARNAME,C,28`,`Total2011,N,20,10`,`Male2011,N,20,10`,`Female2011,N,20,10` FROM `baronies` WHERE `COUNTY,C,5`='33'";
 $query = mysqli_query($connection, $sql);
-
+print($query);
 if (!$query) {
      var_dump($query); 
    /**  echo msqli_error($query); /** This has been commented out for testing**/
