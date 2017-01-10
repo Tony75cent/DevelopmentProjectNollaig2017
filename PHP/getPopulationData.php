d892606e3c563156fcc072b47a2dcb8d17428f7a
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-01-10 13:03:59 +0000

    Further work on getPopulationData.php

--- PHP/getPopulationData.php
+++ PHP/getPopulationData.php
@@ -8,11 +8,14 @@ mysqli_select_db($connection, "$db_name")or die("cannot select a DB");
 /** $sql ="SELECT `COUNTY_ID`, `GEOGDESC`, `CSOBARNAME`, `Male2011`, `Female2011`, `Total2011` FROM `baronys` WHERE `COUNTY_ID`=".$countyid; **/ /** This is the original line **/
 $sql ="`COUNTY,C,5`,`COUNTYNAME,C,35`,`CSOBARNAME,C,28`,`Total2011,N,20,10`,`Male2011,N,20,10`,`Female2011,N,20,10` FROM `baronies` WHERE `COUNTY,C,5`='33'";
 $query = mysqli_query($connection, $sql);
-print($query);
+
 if (!$query) {
-     var_dump($query); 
-   /**  echo msqli_error($query); /** This has been commented out for testing**/
-     die("no viable  query present");  /** 9.01.17 This line is being returned in the console, so therefore the $query set is not being returned, so no graphs being created as a result ? **/
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+   /*  var_dump($query);  */
+   /**  echo msqli_error(); **/ /** This has been commented out for testing**/
+   /*  die("no viable  query present". mysql_error());*/  /** 9.01.17 This line is being returned in the console, so therefore the $query set is not being returned, so no graphs being created as a result ? **/
 }
 
 $data = array(); /* Data returned from the dB will be stored in this array */
